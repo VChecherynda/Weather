bc346dc41159402070a3e51f0a0852b4af393e3b
Vadim Checherynda <checherinda.vadym@gmail.com>
2019-01-14 23:59:57 +0200

    find sibling for title

--- weather.rb
+++ weather.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 uri = "https://www.gismeteo.ua/weather-kyiv-4944/month/"
 doc = Nokogiri::HTML(open(uri))
@@ -8,13 +9,17 @@ doc = Nokogiri::HTML(open(uri))
 
 doc.search('.month_title').each do |month|
   month_title = month.text
-  weeks = month.search('tr')
 
-  weeks.each do |day|
+  weeks_row = month.next_element
+
+  weeks_row.each do |day|
+
+    binding.pry 
+
     day_title = day.search('.day').text
 
     day.each do |item|
-      puts "Weather #{month_title} #{day_title}"
+      puts("Weather #{month_title} #{day_title}")
     end
       # min = item.search('.temp.min m_temp.c')
       # max = item.search('.temp.max m_temp.c')
