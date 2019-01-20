d4813d2969e776abe22e9a9d647077d527f40ff8
Vadim Checherynda <checherinda.vadym@gmail.com>
2019-01-20 12:25:00 +0200

    Replace function names and order of the function

--- weather.rb
+++ weather.rb
@@ -3,38 +3,22 @@ require 'open-uri'
 require 'pry'
 
 uri = "https://www.gismeteo.ua/weather-kyiv-4944/month/"
-doc = Nokogiri::HTML(open(uri))
+@doc = Nokogiri::HTML(open(uri))
 
 @f = File.open('weather.txt','w')
 
-@month_titles = doc.search('.fcontent h3')
-@weather_rows = doc.search('.fcontent table.calendar')
-
-def filter_month_title(title)
-  /[а-я]+/.match(title.downcase)
+def extract_string_from_node(param,css_class)
+  param.search(css_class).text
 end
 
-def generate_month
-  @month_titles.each do |month|
-    month_title = filter_month_title(month.content)
-    generate_rows(month_title)
-  end
+def form_day_temperature(day,month)
+  day_title = extract_string_from_node(day,'.link-day .day')
+  max_temp = extract_string_from_node(day,'.link-day .temp.max .m_temp.c')
+  min_temp = extract_string_from_node(day,'.link-day .temp.min .m_temp.c')
+  @f.write("#{day_title} #{month} , min #{min_temp} C, max #{max_temp} C \n")
 end
 
-def generate_rows(month)
-  @weather_rows.each do |row|
-    generate_weeks(row,month)
-  end
-end
-
-def generate_weeks(row,month)
-  weeks = row.search('tr')
-  weeks.each do |day|
-    generate_day(day,month)
-  end
-end
-
-def generate_day(day,month)
+def day_node_from_nokogiri_by_tag(day,month)
   day_of_the_week = day.search('td')
   day_of_the_week.each do |item|
     if item.search('.link-day .day').text.length > 0
@@ -43,18 +27,37 @@ def generate_day(day,month)
   end
 end
 
-def handle_string(param,css_class)
-  param.search(css_class).text
+def weeks_node_from_nokogiri_by_tag(row,month)
+  weeks = row.search('tr')
+  weeks.each do |day|
+    day_node_from_nokogiri_by_tag(day,month)
+  end
 end
 
-def form_day_temperature(day,month)
-  day_title = handle_string(day,'.link-day .day')
-  max_temp = handle_string(day,'.link-day .temp.max .m_temp.c')
-  min_temp = handle_string(day,'.link-day .temp.min .m_temp.c')
-  @f.write("#{day_title} #{month} , min #{min_temp} C, max #{max_temp} C \n")
+
+
+def extract_month_title_from_string(title)
+  /[а-я]+/.match(title.downcase)
 end
 
-generate_month
+def month_node_from_nokogiri_by_class
+  month_titles = @doc.search('.fcontent h3')
+  month_titles.each do |month|
+    month_title = extract_month_title_from_string(month.content)
+    table_node_from_nokogiri_by_class(month_title)
+  end
+end
+
+
+
+
+
+
+
+
+
+
+ month_node_from_nokogiri_by_class
 
 @f.close
 
